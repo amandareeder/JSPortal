02ff0e23543242d0ffcf3d96e812541a7a737e24
amandareeder <reederamanda08@gmail.com>
2026-01-16 08:19:52 -0700

    Add files via upload

---
+++ 1script.js
@@ -0,0 +1,12 @@
+const today = new Date();
+const year = today.getFullYear();
+// getMonth() (0 for January)
+const month = String(today.getMonth() + 1).padStart(2, '0'); // pad 0 for single number
+const day = String(today.getDate()).padStart(2, '0'); // pad 0 for single number
+
+const customFormattedDate = `${year}-${month}-${day}`;
+
+document.getElementById("current-date").textContent = customFormattedDate;
+
+
+
